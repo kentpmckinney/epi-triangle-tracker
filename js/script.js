5dbeabe73b36a4dbfa9e079a4c532cd2d05fc16e
Kent McKinney <kent.p.mckinney@gmail.com>
2020-01-26 14:11:06 -0800

    implements user interface

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+function check() {
+
+
+
+}
+
+$(document).ready(function(){
+
+  $("#sideA").keyup(function(){ check(); });
+  $("#sideB").keyup(function(){ check(); });
+  $("#sideC").keyup(function(){ check(); });
+
+});
