bd4efeef1abeec7bfea0fc380b7ef74d92ff34f6
Kent McKinney <kent.p.mckinney@gmail.com>
2020-01-26 14:16:35 -0800

    implements business logic

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 
 function check() {
 
+  $("#result").text("");
 
+  let sideA = parseInt($("#sideA").val());
+  let sideB = parseInt($("#sideB").val());
+  let sideC = parseInt($("#sideC").val());
+
+  if (sideA && sideB && sideC) {
+    aEqB = sideA === sideB;
+    aEqC = sideA === sideC;
+    bEqC = sideB === sideC;
+  
+    let equilateral = (aEqB && bEqC);
+    let isoceles = (!equilateral) && (aEqB || aEqC || aEqC);
+    let scalene = !aEqB && !aEqC && !bEqC;
+  
+    if (equilateral) { $("#result").text("This is an equilateral triangle.") }
+    else if (isoceles) { $("#result").text("This is an isosceles triangle.") }
+    else if (scalene) { $("#result").text("This is a scalene triangle.") }
+    else { $("#result").text("This is not a triangle.") }
+  }
+  else { 
+    $("#result").text("This is not a triangle.");
+  }
 
 }
 
