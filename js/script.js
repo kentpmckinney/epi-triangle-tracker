8c9e3df4fff16dcfa2acd12c79479b4461c1e811
Kent McKinney <kent.p.mckinney@gmail.com>
2020-01-26 18:01:21 -0800

    added comments

--- js/script.js
+++ js/script.js
@@ -8,23 +8,28 @@ function check() {
   let sideC = parseInt($("#sideC").val());
 
   if (sideA && sideB && sideC) {
+
+    // Check for side equalities
     aEqB = sideA === sideB;
     aEqC = sideA === sideC;
     bEqC = sideB === sideC;
 
+    // Check for inequality theorem violations
     aGtBC = sideA > (sideB + sideC);
     bGtAC = sideB > (sideA + sideC);
     cGtAB = sideC > (sideA + sideB);
   
-    let equilateral = (aEqB && bEqC);
-    let isoceles = (!equilateral) && (aEqB || aEqC || bEqC);
+    // Determine the type of triangle
+    let equilateral = aEqB && bEqC;
+    let isoceles = !equilateral && (aEqB || aEqC || bEqC);
     let scalene = !aEqB && !aEqC && !bEqC;
+    let inequalityTheorem = aGtBC || bGtAC || cGtAB;
   
-    if (aGtBC || bGtAC || cGtAB) { $("#result").text("This is not a triangle per the inequality theorem.") }
+    // Report the type of triangle to the user interface
+    if (inequalityTheorem) { $("#result").text("This is not a triangle per the inequality theorem.") }
     else if (equilateral) { $("#result").text("This is an equilateral triangle.") }
     else if (isoceles) { $("#result").text("This is an isosceles triangle.") }
     else if (scalene) { $("#result").text("This is a scalene triangle.") }
-    else { $("#result").text("This is not a triangle.") }
   }
 
 }
